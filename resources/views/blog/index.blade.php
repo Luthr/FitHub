7faa5b12465286d28db19e0a368d7ef370edee14
Dean Porter <deanporter_7@hotmail.com>
2017-04-12 12:43:20 +0000

    Finished part 43, working on inline input into blog/post.index + welcome - security risk will be addressed in next lesson

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -13,7 +13,8 @@
         <h2>{{ $post->title }}</h2>
         <h5>Published: {{ date('M j, Y h:ia', strtotime($post->created_at))}}</h5>
 
-        <p>{{ substr($post->body, 0, 250)}}{{strlen($post->body) > 250 ? '...' : ""}} </p>
+        <p>{{ substr(strip_tags($post->body), 0, 250) }} {{ strlen(strip_tags($post->body)) > 250 ? '...' : ""}} </p>
+
         <a href="{{ url('blog/'.$post->slug)}}" class="btn btn-primary">Read More</a>
 
           @foreach ($post->tags as $tag)
--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <h1>{{ $post->title }}</h1>
-      <p>{{ $post->body }}</p>
+      <p>{!! $post->body !!}</p>
       <hr>
       <div class="tags">
         @foreach ($post->tags as $tag)
