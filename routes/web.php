fe78d226d991ca51d9ff0fc4a32d7e697870a8f1
Dean Porter <deanporter_7@hotmail.com>
2017-07-25 22:57:52 +0100

    Disabling register

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,23 @@
 
 Route::group(['middleware' => ['web']], function () {
 
+
+
+
+    // Authentication Routes...
+    Route::get('login', 'Auth\AuthController@showLoginForm');
+    Route::post('login', 'Auth\AuthController@login');
+    Route::get('logout', 'Auth\AuthController@logout');
+
+// Registration Routes... removed!
+
+// Password Reset Routes...
+//    Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+//    Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+//    Route::post('password/reset', 'Auth\PasswordController@reset');
+
+
+
     // ie: domain/blog/slug-goes-here
     Route::get('blog', ['uses' => 'BlogController@index', 'as' => 'blog.index']);
     Route::get('blog/{post}', ['as' => 'blog.single', 'uses' => 'BlogController@show'])
@@ -32,7 +49,7 @@ Route::group(['middleware' => ['web']], function () {
     Route::resource('tags', 'TagController', ['except' => ['create']]);
     Route::resource('content', 'ContentController');
     //shortcut for defining auth/reg/reset routes
-    Auth::routes();
+//    Auth::routes();
 
     Route::put('/update-schedule')->name('schedule.update')->uses('ScheduleController@update')->middleware('auth');
     Route::get('/edit-schedule')->name('schedule.edit')->uses('ScheduleController@edit')->middleware('auth');
